54dbee4d44460137b816772d8efef78c1ba17d56
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-08-08 17:38:31 +0600

    project done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    
+  images: {
+    domains: ['cdn.weatherapi.com'], // Add any other domains you might use as well
+  },
+}
 
 module.exports = nextConfig
